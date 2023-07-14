8a2db18c1e39b0cf8f1ccff8c61687d68ab9b49d
mangleshKumar <77803552+Mangleshkumar665@users.noreply.github.com>
2023-07-14 21:39:55 +0530

    No Bootstraps Carousel

--- app/src/Components/FrontPage.jsx
+++ app/src/Components/FrontPage.jsx
@@ -7,23 +7,23 @@ const FrontPage = () => {
         <>
             <div className=' frontpage-wrapper '>
 
-                <div id="carouselExampleAutoplaying" className="carousel slide " data-bs-ride="carousel ">
-                    <div className="carousel-inner ">
-                        <div className="carousel-item active">
+                <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
+                    <div className="carousel-inner">
+                        <div className="carousel-item active" data-bs-interval="10000">
+                        <img src={img1} className="d-block w-100" alt="..." />
+                        </div>
+                        <div className="carousel-item" data-bs-interval="2000">
+                        <img src={img2} className="d-block w-100" alt="..." />
+                        </div>
+                        <div className="carousel-item">
                             <img src={img1} className="d-block w-100" alt="..." />
                         </div>
-                        <div className="carousel-item">
-                            <img src={img2} className="d-block w-100" alt="..." />
-                        </div>
-                        <div className="carousel-item">
-                            <img src={img2} className="d-block w-100" alt="..." />
-                        </div>
                     </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
                         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span className="visually-hidden">Previous</span>
                     </button>
-                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
                         <span className="carousel-control-next-icon" aria-hidden="true"></span>
                         <span className="visually-hidden">Next</span>
                     </button>
